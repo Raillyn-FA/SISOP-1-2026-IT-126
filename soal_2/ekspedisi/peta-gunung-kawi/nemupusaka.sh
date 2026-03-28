32f152dc8eaaa88dcfc4cf965216b4b87601af48
Raillyn-FA <rayhannxzl@gmail.com>
2026-03-29 02:09:03 +0700

    Update nemupusaka.sh

--- soal_2/ekspedisi/peta-gunung-kawi/nemupusaka.sh
+++ soal_2/ekspedisi/peta-gunung-kawi/nemupusaka.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
 
-# ambil titik pertama dan terakhir (diagonal)
 read first < titik-penting.txt
 read last < <(tail -n 1 titik-penting.txt)
 
-# parsing
 lat1=$(echo $first | awk -F, '{print $3}')
 lon1=$(echo $first | awk -F, '{print $4}')
 
 lat2=$(echo $last | awk -F, '{print $3}')
 lon2=$(echo $last | awk -F, '{print $4}')
 
-# hitung titik tengah
 lat_mid=$(awk "BEGIN {print ($lat1 + $lat2)/2}")
 lon_mid=$(awk "BEGIN {print ($lon1 + $lon2)/2}")
 
-# output
 echo "Koordinat pusaka: ($lat_mid, $lon_mid)" > posisipusaka.txt
 
 cat posisipusaka.txt
